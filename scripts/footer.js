797257036a13926f580c5823b844dbcddcae705c
zehao <zhangzehao0411@gmail.com>
2025-10-13 22:14:57 +0800

    Slight modificaitons on styles.

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,7 @@
 (() => {
   const FOOTER_HTML = `
     <div class="container">
+      <hr style="border: 0; border-top: 1px solid #fff; margin-bottom: 1.2rem; width: 75%; opacity: 0.35;">
       <p>&copy; <span id="year"></span> Zehao Zhang. All rights reserved.</p>
       <p>Last updated October 2025.</p>
     </div>
